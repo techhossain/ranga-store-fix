66bfef5d20e3258bfac10a84a302e76530b65867
Md Hossain <maherhossain10@gmail.com>
2021-09-13 13:17:58 +0600

    fix bugs

--- js/app.js
+++ js/app.js
@@ -10,22 +10,65 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
+    const id = product.id;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+    div.innerHTML = `
+    <div class="col">
+    <div class="card">
+      <img src="${image}" class="card-img-top" alt="${product.title}" height="250">
+      <div class="card-body">
+        <h5 class="card-title">${product.title}</h5>
+        <p>Category: ${product.category}</p>
+        <p>Rating: ${product.rating.count} | Average Rating: ${product.rating.rate}</p>
+        <h2 class="fs-4">Price: $ ${product.price}</h2>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+      <div class="card-footer text-center">
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info">add to cart</button>
+      
+      <button id="details-btn" class="btn btn-dark" onclick='loadSingleProduct(${id})' data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+      </div>
+    </div>
+  </div>`;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+// load single product
+const loadSingleProduct = id => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showSingleProduct(data));
+}
+
+// show single product in UI 
+
+const showSingleProduct = data => {
+  // Clear Previous Item
+  document.getElementById('single-product-details').innerText = '';
+  // Single Item Details
+  const container = document.getElementById('single-product-details');
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+  <div class="card-header">
+    ${data.title}
+  </div>
+  <div class="card-body">
+  <img src="${data.image}" class="card-img-top" alt="${data.title}" height="250">
+    <h5 class="card-text">Category: ${data.category}</h5>
+    <p class="card-text">Price: $ ${data.price}</p>
+    <p class="card-text">Total Rating: <strong>${data.rating.count}</strong> | Average Rating <strong>${data.rating.rate}</strong></p>
+    <p class="card-text">${data.description}</p>
+  </div>
+  `;
+  container.appendChild(card);
+};
+
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -37,7 +80,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,12 +89,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -69,6 +112,8 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+  //  update total cart price
+  updateTotal();
 };
 
 //grandTotal update function
@@ -76,5 +121,14 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// Clear cart after Buy Now 
+const orderProducts = () => {
+  setInnerText('total-Products', 0);
+  setInnerText('price', 0);
+  setInnerText('delivery-charge', 0);
+  setInnerText('total-tax', 0);
+  setInnerText('total', 0);
+}
